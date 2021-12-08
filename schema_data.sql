17bed7bc71eea4e9a94e59605ab83ee2eaf3c3da
WhoRun <473189493@qq.com>
2021-12-08 20:26:00 +0800

    feat: 连通数据库。

--- schema_data.sql
+++ schema_data.sql
@@ -3,8 +3,8 @@
   */
 
 
-/* CREATE TABLE persons (id  integer, create_datetime datetime, email varchar(255), phone varchar(255), sex varchar(255), username varchar(255), zone blob, primary key (id));
-*/
+CREATE TABLE persons (id  integer, create_datetime datetime, email varchar(255), phone varchar(255), sex varchar(255), username varchar(255), zone blob, primary key (id));
+
 
 
 INSERT INTO persons (create_datetime, email, phone, sex, username, zone) VALUES (datetime('now'), 'gubaoer@hotmail.com', 08613000001111, 'male', 'gubaoer', 'HongKou District');
